91ffbf24f85e8ce3cd243c905ebc7d6f61c58bb4
trebletoxin <hopkinsjasen@gmail.com>
2026-02-07 12:50:46 -0500

    Update main.py
    Removed -RZ flag to keep compatibility with bridge consistent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests, json, asyncio, uuid, aiohttp, sys, argparse, os, platform, subprocess, shutil, unicodedata, re
 
-async def downloadChart(tempFolder, chartFolder, theChart: dict, rzflag) -> str:
+async def downloadChart(tempFolder, theChart: dict) -> str:
 	url = f"https://files.enchor.us/{theChart['md5']}{('_novideo','')[not theChart['hasVideoBackground']]}.sng"
 	async with aiohttp.ClientSession() as session:
 		custom_timeout = aiohttp.ClientTimeout(sock_connect=10, total=300)
@@ -19,7 +19,6 @@ async def downloadChart(tempFolder, chartFolder, theChart: dict, rzflag) -> str:
 
 	sngUuid = str(uuid.uuid4())
 	sngScratchDir = os.path.join(tempFolder, sngUuid)
-	output = outputChartDir(chartFolder, theChart, rzflag)
 
 	try:
 		os.makedirs(sngScratchDir)
@@ -33,7 +32,7 @@ async def downloadChart(tempFolder, chartFolder, theChart: dict, rzflag) -> str:
 
 	return sngScratchDir
 
-async def convertChart(tempFolder, chartFolder, theChart, rzflag) -> bool:
+async def convertChart(tempFolder, chartFolder, theChart) -> bool:
 	sngCliPath = f".\\SngCli\\SngCli.exe" if platform.system() == 'Windows' else f"SngCli/SngCli"
 	try:
 		outputDir = os.path.join(tempFolder,"1")
@@ -42,7 +41,7 @@ async def convertChart(tempFolder, chartFolder, theChart, rzflag) -> bool:
 		print(f"SngCli Decode Failed: {e}")
 		return False
 	
-	outputFolder = outputChartDir(chartFolder, theChart, rzflag)
+	outputFolder = outputChartDir(chartFolder, theChart)
 	if platform.system() == "Windows":
 		finalChartPath = f"{u'\\\\?\\'}{outputFolder['dir']}"
 	else:
@@ -75,8 +74,7 @@ def trimPageDuplicates(thePage) -> dict:
 				del thePage[j]
 	return thePage
 
-def outputChartDir(chartFolder, theChart: str, rzflag) -> dict:
-	MAX_FILE_LEN = os.pathconf('.', 'PC_NAME_MAX') if platform.system() != "Windows" else 255
+def outputChartDir(chartFolder, theChart: str) -> dict:
 	newFile = f"{theChart['artist']} - {theChart['name']} ({theChart['charter']})"
 	newFile = newFile.replace("/",  u'\uFF0F') #／
 	newFile = newFile.replace("\\", u'\u29F5') #⧵
@@ -87,22 +85,18 @@ def outputChartDir(chartFolder, theChart: str, rzflag) -> dict:
 	newFile = newFile.replace("?",  u'\uFF1F') #？
 	newFile = newFile.replace("*",  u'\u204E') #⁎
 	newFile = newFile.replace("|",  u'\u23D0') #⏐
-	newFile = newFile.lstrip()
-	if rzflag:
-		newFile = newFile.replace(u'\u200b', "")
-		newFile = newFile.replace(u'\u200c', "")
 	newFile = newFile.strip()
 
 	encoding = 'utf-8'
 	bytes_data = newFile.encode(encoding)
-	sliced_bytes = bytes_data[:MAX_FILE_LEN]
+	sliced_bytes = bytes_data[:255]
 	newFile = sliced_bytes.decode(encoding, errors='ignore')
 	newFile = newFile.rstrip()
 	outputDir = f"{chartFolder}\\{newFile}"
     
 	return { "dir" : outputDir, "file" : newFile }
 
-def oldOutputChartDir(chartFolder, theChart: str, rzflag) -> dict:
+def oldOutputChartDir(chartFolder, theChart: str) -> dict:
 	MAX_FILE_LEN = os.pathconf('.', 'PC_NAME_MAX') if platform.system() != "Windows" else 255
 	newFile = f"{theChart['artist']} - {theChart['name']} ({theChart['charter']})"
 	newFile = newFile.replace("/",  "")
@@ -114,10 +108,6 @@ def oldOutputChartDir(chartFolder, theChart: str, rzflag) -> dict:
 	newFile = newFile.replace("?",  "")
 	newFile = newFile.replace("*",  "")
 	newFile = newFile.replace("|",  "")
-	newFile = newFile.lstrip()
-	if rzflag:
-		newFile = newFile.replace(u'\u200b', "")
-		newFile = newFile.replace(u'\u200c', "")
 	newFile = newFile.strip()
 
 	if platform.system() == 'Windows':
@@ -133,7 +123,7 @@ def oldOutputChartDir(chartFolder, theChart: str, rzflag) -> dict:
 async def doChartDownload(theChart, args, sema):
 	async with sema:
 		print(f"Starting download/conversion of chart {theChart['name']} - {theChart['album']} - {theChart['artist']} - {theChart['charter']} - {theChart['md5']}")
-		tempFolder = await downloadChart(args.temp_directory, args.clone_hero_folder, theChart, args.remove_zerowidth)
+		tempFolder = await downloadChart(args.temp_directory, theChart)
 		if not tempFolder:
 			print(f"Error downloading chart {theChart['name']} - {theChart['album']} - {theChart['artist']} - {theChart['md5']}")
 			if args.stop_on_error:
@@ -142,14 +132,14 @@ async def doChartDownload(theChart, args, sema):
 			else:
 				return
 
-		if not await convertChart(tempFolder, args.clone_hero_folder, theChart, args.remove_zerowidth):
+		if not await convertChart(tempFolder, args.clone_hero_folder, theChart):
 			print(f"Error converting chart {theChart['name']} - {theChart['album']} - {theChart['artist']} - {theChart['md5']}")
 			if args.stop_on_error:
 				print("Encountered error, and continue error not set, quitting.")
 				sys.exit(1)
 
 		if args.remove_playlist:
-			chartDir = outputChartDir(args.clone_hero_folder, theChart, args.remove_zerowidth)['dir'] if platform.system() != 'Windows' else f"{u'\\\\?\\'}{outputChartDir(args.clone_hero_folder, theChart, args.remove_zerowidth)['dir']}"
+			chartDir = outputChartDir(args.clone_hero_folder, theChart)['dir'] if platform.system() != 'Windows' else f"{u'\\\\?\\'}{outputChartDir(args.clone_hero_folder, theChart)['dir']}"
 			if os.path.isfile(os.path.join(chartDir, "song.ini")):
 				await removePlaylist(chartDir)
 
@@ -163,41 +153,16 @@ async def removePlaylist(chartDir):
 			if "playlist_track=" not in lineTest and "playlist=" not in lineTest:
 				file.write(line)
 
-def renameZeroWidthFolders(chFolder):
-	folders = [f for f in os.listdir(chFolder) if os.path.isdir(os.path.join(chFolder, f))]
-	filtered_list = [item for item in folders if u'\u200b' in item or u'\u200c' in item]
-	for folder in filtered_list:
-		renamedFolder = folder.replace(u'\u200b', "")
-		renamedFolder = renamedFolder.replace(u'\u200c', "")
-		renamedFolder = renamedFolder.strip()
-		oldFolder = os.path.join(chFolder, folder)
-		newFolder = os.path.join(chFolder, renamedFolder)
-		if platform.system() == "Windows":
-			oldFolder = f'{u'\\\\?\\'}{oldFolder}'
-			newFolder = f'{u'\\\\?\\'}{newFolder}'
-		if os.path.isdir(newFolder):
-			shutil.rmtree(oldFolder)
-		else:
-			shutil.move(oldFolder, newFolder)
-			shutil.rmtree(oldFolder)
-
 def schemaRename(chFolder, theChart):
 	if platform.system != "Windows":
 		oldDir = oldOutputChartDir(chFolder, theChart, False)['dir']
 		newDir = outputChartDir(chFolder, theChart, False)['dir']
-		oldDirRZ = oldOutputChartDir(chFolder, theChart, True)['dir']
-		newDirRZ = outputChartDir(chFolder, theChart, True)['dir']
 	else:
 		oldDir = f"{u'\\\\?\\'}{oldOutputChartDir(chFolder, theChart, False)['dir']}"
 		newDir = f"{u'\\\\?\\'}{outputChartDir(chFolder, theChart, False)['dir']}"
-		oldDirRZ = f"{u'\\\\?\\'}{oldOutputChartDir(chFolder, theChart, True)['dir']}"
-		newDirRZ = f"{u'\\\\?\\'}{outputChartDir(chFolder, theChart, True)['dir']}"
 	if os.path.isdir(oldDir) and oldDir != newDir:
 		print(f'Renaming improperly named chart folder: {oldDir}')
 		shutil.move(oldDir,newDir)
-	if os.path.isdir(oldDirRZ) and oldDirRZ != newDirRZ:
-		print(f'Renaming improperly named chart folder: {oldDirRZ}')
-		shutil.move(oldDirRZ,newDirRZ)
 
 def main():
 	argParser = argparse.ArgumentParser()
@@ -208,7 +173,6 @@ def main():
 	argParser.add_argument("-soe", "--stop-on-error", help="Continue on error during conversion or download", action="store_true")
 	argParser.add_argument("-chf", "--clone-hero-folder", help="Clone Hero songs folder to output charts to", required=True)
 	argParser.add_argument("-rp", "--remove-playlist", help="Remove playlist data for downloaded charts", action="store_true")
-	argParser.add_argument("-rz", "--remove-zerowidth", help="BREAKS BRIDGE COMPATIBILITY! Removes zero-width characters from chart names. Retroactively renames any chart folders that contain zero-width characters.", action="store_true")
 	argParser.add_argument("-d", "--charts-with-drums", help="Only downloads charts containing drum", action="store_true")
 	argParser.add_argument("-sc", "--schema-cleanup", help="Renames folders that do not match Bridge's naming schema", action="store_true")
 	args = argParser.parse_args()
@@ -223,9 +187,6 @@ def main():
 		print("Will stop download/convert of charts on error")
 	if args.remove_playlist:
 		print("Removing playlist data for charts (downloaded+to-download)")
-	if args.remove_zerowidth:
-		print("Removing zero-width characters from chart folder names (downloaded+to-download)")
-		renameZeroWidthFolders(args.clone_hero_folder)
 	if args.schema_cleanup:
 		print("Renaming old chart folders to new naming schema")
 
@@ -242,8 +203,8 @@ def main():
 				print(f"Progress {chartNum} of {numCharts}")
 			if args.schema_cleanup:
 				schemaRename(args.clone_hero_folder, chart)
-			oldChartDir = oldOutputChartDir(args.clone_hero_folder, chart, args.remove_zerowidth)['dir'] if platform.system() != 'Windows' else f"{u'\\\\?\\'}{oldOutputChartDir(args.clone_hero_folder, chart, args.remove_zerowidth)['dir']}"
-			chartDir = outputChartDir(args.clone_hero_folder, chart, args.remove_zerowidth)['dir'] if platform.system() != 'Windows' else f"{u'\\\\?\\'}{outputChartDir(args.clone_hero_folder, chart, args.remove_zerowidth)['dir']}"
+			oldChartDir = oldOutputChartDir(args.clone_hero_folder, chart)['dir'] if platform.system() != 'Windows' else f"{u'\\\\?\\'}{oldOutputChartDir(args.clone_hero_folder, chart)['dir']}"
+			chartDir = outputChartDir(args.clone_hero_folder, chart)['dir'] if platform.system() != 'Windows' else f"{u'\\\\?\\'}{outputChartDir(args.clone_hero_folder, chart)['dir']}"
 			if os.path.isdir(chartDir) or os.path.isdir(oldChartDir):
 				if args.remove_playlist and os.path.isfile(os.path.join(chartDir, "song.ini")):
 					asyncio.run(removePlaylist(chartDir))
